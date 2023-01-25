5b47a691eb161b6ef7bce45362e63f63d1ecce83
Malinda-Madhumal <malinda0702207@gmail.com>
2023-01-25 18:20:09 +0530

    first commit

--- firebase.js
+++ firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAu9-1nY-Mqmh_vNqyy1LUr9-SOdz6FUks",
-  authDomain: "covid-clone-5fd62.firebaseapp.com",
-  projectId: "covid-clone-5fd62",
-  storageBucket: "covid-clone-5fd62.appspot.com",
-  messagingSenderId: "140980376309",
-  appId: "1:140980376309:web:20713162edc41e5f9a6bbc",
-  measurementId: "G-MDLX44HMDZ",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 const app = initializeApp(firebaseConfig);
